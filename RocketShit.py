e09248d12c55b027175beacdbeefabf26c6102c2
Jaine <jaine.turban@cooper.edu>
2025-12-08 15:49:46 -0500

    Setting up variables

--- RocketShit.py
+++ RocketShit.py
@@ -1,4 +1,13 @@
-x=2 
-y=3 
-z=x*y
-print(z)
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Defining Variables
+m_0= 1000  #Rocket's intitial mass in kg
+v_0= 343 #Rocket's initial speed in m/s
+g=9.81 #gravity m/s^2
+#t= function for finding time 
+t=((((2)^1/2)-1)/(g))*v_0
+dm/dt -1500 #kg/s
+#dv/dt
+#v #T_eff(t,h)-Effective Thrust
+T_eff=(m*(dv/dt))+(dm/dt)*v
